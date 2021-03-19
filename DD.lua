a9800c754664903d050f41ec637f3ce8e84c0223
leafeon012 <77223325+leafeon012@users.noreply.github.com>
2021-03-19 22:59:12 +0900

    Add files via upload

---
+++ DD.lua
@@ -0,0 +1,160 @@
+gg.setVisible(false)
+
+local Passwords = {"냥코레인저스","박사"}
+local Menu = gg.prompt({"인증 코드를 입력해주십시오"},nil,{"text"})
+if not Menu then 
+return 
+end
+for l , I in pairs(Passwords) do
+if Menu[1] == I then A = true
+end
+end
+if A ~= true then gg.alert("인증에 실패했습니다.")
+return
+else
+gg.alert("인증에 성공했습니다.")
+end
+
+gg.alert(os.date([[시노노메 박사 V.10.3.0
+
+- 온라인 서버에서 지속적으로 업데이트 됩니다.
+- 오류 전달은 냥코레인저스 갤러리로 부탁드립니다.]]))
+HOMEDM = 1
+function Main()
+menu= gg.choice({
+"XP 개수 조작",
+"통조림 개수 조작",
+"아이템 개수 조작",
+"전투 즉시 승리",
+"스크립트 종료"}, nil, ([[현재 최신버전입니다.]]))
+if menu == nil then
+else
+if menu == 1 then T1() end
+if menu == 2 then T2() end
+if menu == 3 then T3() end
+if menu == 4 then T4() end
+if menu == 5 then T5() end
+end
+vis= false
+end
+
+function T1()
+harpp = gg.prompt({'원하는 수치를 입력해주세요 (99999999개 미만으로)'}, nil, {'number'})
+if harpp == nil then
+else
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearList()
+gg.clearResults()
+gg.searchNumber("1,179,403,647;65,793;196,611;1;52;9,448,224;2,097,204;2,621,448;1,835,037:49", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1,179,403,647", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.processPause()
+local t = gg.getResults(7)
+gg.addListItems(t)
+local t3 = {}
+for i, v in ipairs(t) do
+	table.insert(t3, {address=v.address+0x909E20,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x909E24,flags=gg.TYPE_DWORD,value=0,freeze=false})
+end
+gg.addListItems(t3)
+revert = gg.getListItems()
+gg.setValues(t3)
+gg.processResume()
+gg.toast("적용이 완료되었습니다.")
+end
+end
+
+function T2()
+harpp = gg.prompt({'원하는 수치를 입력해주세요 (20000개 미만으로)'}, nil, {'number'})
+if harpp == nil then
+else
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearList()
+gg.clearResults()
+gg.searchNumber("1,179,403,647;65,793;196,611;1;52;9,448,224;2,097,204;2,621,448;1,835,037:49", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1,179,403,647", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.processPause()
+local t = gg.getResults(7)
+gg.addListItems(t)
+local t3 = {}
+for i, v in ipairs(t) do
+	table.insert(t3, {address=v.address+0x909D34,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x909D38,flags=gg.TYPE_DWORD,value=0,freeze=false})
+end
+gg.addListItems(t3)
+revert = gg.getListItems()
+gg.setValues(t3)
+gg.processResume()
+gg.toast("적용이 완료되었습니다.")
+end
+end
+
+function T3()
+harpp = gg.prompt({'원하는 수치를 입력해주세요 (999개 미만으로)'}, nil, {'number'})
+if harpp == nil then
+else
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearList()
+gg.clearResults()
+gg.searchNumber("1,179,403,647;65,793;196,611;1;52;9,448,224;2,097,204;2,621,448;1,835,037:49", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1,179,403,647", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.processPause()
+local t = gg.getResults(7)
+gg.addListItems(t)
+local t3 = {}
+for i, v in ipairs(t) do
+	table.insert(t3, {address=v.address+0x90E1F8,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x90E1FC,flags=gg.TYPE_DWORD,value=0,freeze=false})
+	table.insert(t3, {address=v.address+0x90E200,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x90E204,flags=gg.TYPE_DWORD,value=0,freeze=false})
+	table.insert(t3, {address=v.address+0x90E208,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x90E20C,flags=gg.TYPE_DWORD,value=0,freeze=false})
+	table.insert(t3, {address=v.address+0x90E210,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x90E214,flags=gg.TYPE_DWORD,value=0,freeze=false})
+	table.insert(t3, {address=v.address+0x90E218,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x90E21C,flags=gg.TYPE_DWORD,value=0,freeze=false})
+	table.insert(t3, {address=v.address+0x90E220,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
+	table.insert(t3, {address=v.address+0x90E224,flags=gg.TYPE_DWORD,value=0,freeze=false})
+end
+gg.addListItems(t3)
+revert = gg.getListItems()
+gg.setValues(t3)
+gg.processResume()
+gg.toast("적용이 완료되었습니다.")
+end
+end
+
+function T4()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.clearList()
+gg.clearResults()
+gg.searchNumber("1,179,403,647;65,793;196,611;1;52;9,448,224;2,097,204;2,621,448;1,835,037:49", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("1,179,403,647", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+local t = gg.getResults(7)
+gg.addListItems(t)
+local t3 = {}
+for i, v in ipairs(t) do
+table.insert(t3, {address=v.address+0x93CB6C,flags=gg.TYPE_DWORD,value=0,freeze=false})
+end
+gg.addListItems(t3)
+revert = gg.getListItems()
+gg.setValues(t3)
+gg.toast("적용이 완료되었습니다.")
+end
+
+function T5()
+print([[오류 전달은 냥코레인저스 갤러리로 부탁드립니다.]])
+gg.clearResults()
+gg.clearList()
+gg.setVisible(true)
+os.exit()
+end
+
+while (true) do
+if gg.isVisible(true) then
+vis= true
+gg.setVisible(false)
+end
+if vis then
+Main()
+end
+end
