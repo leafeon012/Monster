7446b2096e15aa2519181d578a65a3056b483f78
leafeon012 <77223325+leafeon012@users.noreply.github.com>
2021-03-25 18:09:20 +0900

    Update DD.lua

--- DD.lua
+++ DD.lua
@@ -25,7 +25,7 @@ gg.alert([[2021.03.25 업데이트 내역
 - 이 시간 이후로 모바일 64비트만 지원합니다.
 - 에뮬레이터는 지원하지 않습니다.
 - 냥코 3단 진화기 기능이 추가 되었습니다.
-- 특정 기기에서 작동하지 않는 오류가 수정 되었습니다.]])
+- 개다래 개수 조작 기능은 삭제 되었습니다.]])
 
 java1 = io.open("/sdcard/RemakePackWithList.java")
 java2 = io.open("/sdcard/GetPackFromList.java")
@@ -1316,7 +1316,7 @@ local menu= gg.choice({
 "XP 개수 조작",
 "통조림 개수 조작",
 "아이템 개수 조작",
-"개다래 개수 조작",
+"개다래 개수 조작 (지원 종료)",
 "스토리 모드 올클리어",
 "냥코 클럽 시간 조작",
 "티켓 개수 조작",
@@ -1493,72 +1493,7 @@ end
 
 
 function Apple()
-harpp = gg.prompt({'원하는 수치를 입력해주세요 (8개 미만으로)'}, nil, {'number'})
-if harpp == nil then
-else
-gg.setRanges(gg.REGION_OTHER)
-gg.clearList()
-gg.clearResults()
-gg.searchNumber("524,551;1;2;3;4;5;6;7;8;9;10;11;12;13;14::129", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.processPause()
-local t = gg.getResults(70)
-gg.addListItems(t)
-local t3 = {}
-for i, v in ipairs(t) do
-	table.insert(t3, {address=v.address-0x44,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x3C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x34,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x2C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x24,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x1C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x14,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0xC,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address-0x4,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address+0x4,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address+0xC,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address+0x14,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address+0x1C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address+0x24,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t3, {address=v.address+0x2C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-end
-gg.addListItems(t3)
-revert = gg.getListItems()
-gg.setValues(t3)
-gg.processResume()
-
-gg.setRanges(gg.REGION_C_ALLOC)
-gg.clearList()
-gg.clearResults()
-gg.searchNumber("9;0;10;0;11;0;12;0::29", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-gg.processPause()
-local f = gg.getResults(70)
-gg.addListItems(f)
-local t4 = {}
-for i, v in ipairs(f) do
-	table.insert(t4, {address=v.address-0x44,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x3C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x34,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x2C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x24,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x1C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x14,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0xC,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address-0x4,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address+0x4,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address+0xC,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address+0x14,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address+0x1C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address+0x24,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-	table.insert(t4, {address=v.address+0x2C,flags=gg.TYPE_DWORD,value=harpp[1],freeze=false})
-end
-gg.addListItems(t4)
-revert = gg.getListItems()
-gg.setValues(t4)
-gg.processResume()
-gg.toast("적용이 완료되었습니다.")
-end
+gg.alert("지원이 종료된 가능입니다.")
 end
 
 function All()
