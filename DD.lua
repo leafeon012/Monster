4e2e4a8786bae80f5ee62b8c8252230539cce9eb
leafeon012 <77223325+leafeon012@users.noreply.github.com>
2021-03-26 16:05:46 +0900

    Update DD.lua

--- DD.lua
+++ DD.lua
@@ -4,11 +4,12 @@ ip= gg.makeRequest('https://docterhakase.000webhostapp.com/encs.php').content
 if ip == "no"
 then
 gg.setVisible(false)
-gg.alert("접속이 불가능한 아이피입니다.")
+gg.alert("등록이 되지 않은 아이피입니다.")
 gg.setVisible(true)
 os.exit()
 else
 gg.setVisible(false)
+gg.alert("정상적으로 등록된 아이피입니다.")
 end
 
 gg.alert(os.date([[박사 치트 V.10.3.0
