acebb36f03422e7aa319438df84d3be3ddafeb63
leafeon012 <77223325+leafeon012@users.noreply.github.com>
2021-04-01 00:51:07 +0900

    Update DD.lua

--- DD.lua
+++ DD.lua
@@ -4,8 +4,8 @@ gg.clearResults()
 gg.clearList()
 gg.alert("APK 파일 다운로드를 시작합니다.")
 gg.toast("파일을 다운로드중입니다...")
-data = gg.makeRequest("https://docterhakase.000webhostapp.com/NewGG.zip").content
-io.open("/sdcard/New.zip","w+"):write(data):close();
+datas = gg.makeRequest("https://docterhakase.000webhostapp.com/NewGG.zip").content
+io.open("/sdcard/New.zip","w+"):write(datas):close();
 gg.alert([[다운로드가 완료되었습니다.
 /storage/emulated/0/New.zip 파일을 확인하세요.]])
 
