9cd92bffdf59f34721f3fedd01621c2219c7574e
leafeon012 <77223325+leafeon012@users.noreply.github.com>
2021-04-01 11:02:35 +0900

    Create ip.js

---
+++ ip.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+	function getIP(json) {
+		document.write(json.ip);
+	}
+</script>
+<script type="text/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
