4511d778804471c153baf883cf065cc0c8a75666
leafeon012 <77223325+leafeon012@users.noreply.github.com>
2021-04-05 15:38:23 +0900

    Delete ip.js

--- ip.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-	function getIP(json) {
-		document.write(json.ip);
-	}
-</script>
-<script type="text/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
